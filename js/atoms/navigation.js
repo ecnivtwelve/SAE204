a6a8001ed164fd6eda38e7ecd4bf2592b3c55712
Vince Linise <vincelinise@gmail.com>
2025-03-12 14:25:16 +0100

    feat: ajuster le seuil de défilement pour l'ajout de la classe "scroll"

--- js/atoms/navigation.js
+++ js/atoms/navigation.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 20) {
+  if (window.scrollY > 5) {
     document.body.classList.add("scroll");
   } else {
     document.body.classList.remove("scroll");
